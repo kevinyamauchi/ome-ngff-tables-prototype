8e7d6f5a93e7f4fc5dc0abfd07d9888858ae45f4
giovp <giov.pll@gmail.com>
2022-05-01 18:21:54 +0200

    add writer

--- src/ngff_tables_prototype/writer.py
+++ src/ngff_tables_prototype/writer.py
@@ -1,51 +1,65 @@
-from typing import Optional, Union, Tuple, Any, List, Dict
+from typing import Optional, Union, Tuple, Any, List
 
 import anndata
 from anndata import AnnData
-from anndata._io.utils import write_attribute
 import numpy as np
 import pandas as pd
 from ome_zarr.io import parse_url
 from ome_zarr.writer import write_image, write_labels
-from scipy import sparse
 import zarr
-
+from anndata.experimental import write_elem
 from .table_utils import df_to_anndata
 
+# make function that writes region table
+# function that writes region circles
 
-def write_table(
-        group: zarr.Group,
-        adata: AnnData,
-        chunks=None,
-        **dataset_kwargs,
+
+def write_table_points(
+    group: zarr.Group,
+    adata: AnnData,
+    table_group_name: str = "points_table",
+    group_type: str = "ngff:points_table",
 ):
-    """code from: https://github.com/theislab/anndata/blob/0.7.8/anndata/_io/zarr.py
-    """
-    # AnnData requires an upper case key for X
-    # todo: make context manager?
-    group.store.normalize_keys = False
+    write_elem(group, table_group_name, adata)
+    table_group = group[table_group_name]
+    table_group.attrs["@type"] = group_type
 
-    if adata.raw is not None:
-        adata.strings_to_categoricals(adata.raw.var)
-    # TODO: Use spec writing system for this
-    group.attrs.setdefault("encoding-type", "anndata")
-    group.attrs.setdefault("encoding-version", "0.1.0")
-    if chunks is not None and not isinstance(adata.X, sparse.spmatrix):
-        write_attribute(group, "X", adata.X, dict(chunks=chunks, **dataset_kwargs))
-    else:
-        write_attribute(group, "X", adata.X, dataset_kwargs)
-    write_attribute(group, "obs", adata.obs, dataset_kwargs)
-    write_attribute(group, "var", adata.var, dataset_kwargs)
-    write_attribute(group, "obsm", adata.obsm, dataset_kwargs)
-    write_attribute(group, "varm", adata.varm, dataset_kwargs)
-    write_attribute(group, "obsp", adata.obsp, dataset_kwargs)
-    write_attribute(group, "varp", adata.varp, dataset_kwargs)
-    write_attribute(group, "layers", adata.layers, dataset_kwargs)
-    write_attribute(group, "uns", adata.uns, dataset_kwargs)
-    write_attribute(group, "raw", adata.raw, dataset_kwargs)
 
-    # revert the store to normalizing keys
-    group.store.normalize_keys = True
+def write_table_polygons(
+    group: zarr.Group,
+    adata: AnnData,
+    table_group_name: str = "shapes_table",
+    group_type: str = "ngff:shapes_table",
+):
+    pass
+
+
+def write_table_circles(
+    group: zarr.Group,
+    adata: AnnData,
+    table_group_name: str = "circles_table",
+    group_type: str = "ngff:circles_table",
+):
+    write_elem(group, table_group_name, adata)
+    table_group = group[table_group_name]
+    table_group.attrs["@type"] = group_type
+
+
+def write_table_regions(
+    group: zarr.Group,
+    adata: AnnData,
+    table_group_name: str = "regions_table",
+    group_type: str = "ngff:regions_table",
+    region: Union[str, List[str]] = "features",
+    region_key: Optional[str] = None,
+    instance_key: Optional[str] = None,
+):
+    write_elem(group, table_group_name, adata)
+    table_group = group[table_group_name]
+    table_group.attrs["@type"] = group_type
+    table_group.attrs["region"] = region
+    table_group.attrs["region_key"] = region_key
+    table_group.attrs["instance_key"] = instance_key
 
 
 def write_points_dataset(
@@ -56,7 +70,7 @@ def write_points_dataset(
     points: Optional[pd.DataFrame] = None,
     points_dense_columns: Optional[List[str]] = None,
     points_var: Optional[pd.DataFrame] = None,
-    points_group_name: str = 'points'
+    points_group_name: str = "points",
 ):
     """Write a dataset with image and points to ome-zarr
 
@@ -90,39 +104,40 @@ def write_points_dataset(
 
     if image is not None:
         # if necessary add the image
-        write_image(
-            image=image,
-            group=root,
-            chunks=image_chunks,
-            axes=image_axes
-        )
+        write_image(image=image, group=root, chunks=image_chunks, axes=image_axes)
 
     if points is not None:
         points_group = root.create_group(points_group_name)
         points_anndata = df_to_anndata(
-            df=points,
-            dense_columns=points_dense_columns,
-            var=points_var
+            df=points, dense_columns=points_dense_columns, var=points_var
         )
         write_table(points_group, points_anndata)
 
         # add a flag to the group attrs to denote this is points data
-        points_group.attrs['@type'] = 'ngff:points'
+        points_group.attrs["@type"] = "ngff:points"
 
 
 def write_spatial_anndata(
     file_path: str,
-    image: Optional[np.ndarray]=None,
+    # image group
+    image: Optional[np.ndarray] = None,
     image_chunks: Union[Tuple[Any, ...], int] = None,
     image_axes: Union[str, List[str]] = None,
-    label_image: Optional[np.ndarray]=None,
-    label_group_name: str = "labels",
-    label_name: str = "labels",
-    label_column : Optional[str] = "cell_id",
-    adata: Optional[anndata.AnnData] =None,
-    table_group_name: str = "table",
+    # label group
+    label_image: Optional[np.ndarray] = None,
+    # label_name: str = "label_image",
+    # table group
+    tables_adata: Optional[AnnData] = None,
+    tables_region: Optional[Union[str, List[str]]] = None,
+    tables_region_key: Optional[str] = None,
+    tables_instance_key: Optional[str] = None,
+    # shape group
+    circles_adata: Optional[AnnData] = None,
+    polygons_adata: Optional[AnnData] = None,
+    # points group
+    points_adata: Optional[AnnData] = None,
 ):
-    """ Write a spatial anndata object to ome-zarr
+    """Write a spatial anndata object to ome-zarr
     Parameters
     ----------
     file_path : str
@@ -134,6 +149,26 @@ def write_spatial_anndata(
         Chunking for the image data. See ome-zarr-py for details.
     image_axes : Union[str, List[str]]
         The labels for the image axes. See ome-zarr-py for details.
+    label_image : Union[str, List[str]]
+        The label image (raster-mask). See ome-zarr-py for details.
+    tables_adata:
+        The :class:`anndata.AnnData` table with gene expression and annotations.
+    tables_region
+        The :class:`anndata.AnnData` region table that maps to one (or more) label image.
+    tables_region_key
+        The key in :attr:`AnnData.obs` that stores unique pointers to one (or more) label image.
+    tables_instance_key
+        The key in :attr:`AnnData.obs` that stores unique pointers to regions.
+    circles_adata
+        The :class:`anndata.AnnData` circle table that store coordinates of circles and metadata.
+    polygons_adata
+        The :class:`anndata.AnnData` polygons table that store coordinates of polygons and metadata.
+    points_adata
+        The :class:`anndata.AnnData` point table that store coordinates of points and metadata.
+
+    Returns
+    -------
+    Nothing, save the file in Zarr store.
     """
     # create the zarr root
     store = parse_url(file_path, mode="w").store
@@ -141,27 +176,32 @@ def write_spatial_anndata(
 
     if image is not None:
         # if necessary add the image
-        write_image(
-            image=image,
-            group=root,
-            chunks=image_chunks,
-            axes=image_axes
-        )
+        write_image(image=image, group=root, chunks=image_chunks, axes=image_axes)
 
     if label_image is not None:
         # label_image_group = root.create_group(label_group_name)
-        write_labels(
-            label_image,
+        write_labels(label_image, group=root, name=label_name)
+
+    if tables_adata is not None:
+        write_table_regions(
+            tables_adata,
             group=root,
-            name=label_name
+            tables_region=tables_region,
+            tables_region_key=tables_region_key,
+            tables_instance_key=tables_instance_key,
         )
-
-    if adata is not None:
-        from anndata.experimental import write_elem
-
-        # write_table(table_group, adata)
-        write_elem(root, table_group_name, adata)
-        table_group = root[table_group_name]
-        table_group.attrs["@type"] = "ngff:table"
-        table_group.attrs["label"] = label_group_name
-        table_group.attrs["label_column"] = label_column
+    if circles_adata is not None:
+        write_table_regions(
+            circles_adata,
+            group=root,
+        )
+    if polygons_adata is not None:
+        write_table_regions(
+            polygons_adata,
+            group=root,
+        )
+    if points_adata is not None:
+        write_table_regions(
+            points_adata,
+            group=root,
+        )
