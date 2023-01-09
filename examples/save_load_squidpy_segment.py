72738452fadb87bd5f7a7698bc4511836b43a28e
William Moore <w.moore@dundee.ac.uk>
2023-01-09 16:11:34 +0000

    Fix dtype in save_load_squidpy_segment.py before writing

--- examples/save_load_squidpy_segment.py
+++ examples/save_load_squidpy_segment.py
@@ -9,6 +9,15 @@ import numpy as np
 output_fpath = "test_segment.zarr"
 
 
+def fix_dtype_int64(adata):
+    # to allow viewing in web, convert int64 > int32 where possible:
+    max_32 = np.iinfo(np.int32).max
+    # currently just do 'obs' but could do others as needed
+    for key in adata.obs_keys():
+        if adata.obs[key].dtype == np.int64 and adata.obs[key].max() < max_32:
+            adata.obs[key] = adata.obs[key].astype(np.int32)
+
+
 def write_segmentation_adata() -> None:
     adata = sq.datasets.mibitof()
     lib_id = "point8"
@@ -18,6 +27,8 @@ def write_segmentation_adata() -> None:
     segment = adata.uns[spatial_key][lib_id]["images"]["segmentation"]
     adata.X = adata.X.A.copy()
     tables_adata = adata.copy()
+
+    fix_dtype_int64(tables_adata)
 
     if True:
         if os.path.isdir(output_fpath):
